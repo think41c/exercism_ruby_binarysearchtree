9a43e7125d074b2d1a8b59f05f12b80a92253a99
Tony DiNitto <think41c@gmail.com>
2015-05-08 18:02:39 -0400

    All tests passing.

--- binary_search_tree_test.rb
+++ binary_search_tree_test.rb
@@ -53,26 +53,22 @@ class BstTest < Minitest::Test
   end
 
   def test_iterating_one_element
-    skip
     assert_equal [4], record_all_data(Bst.new 4)
   end
 
   def test_iterating_over_smaller_element
-    skip
     four = Bst.new 4
     four.insert 2
     assert_equal [2, 4], record_all_data(four)
   end
 
   def test_iterating_over_larger_element
-    skip
     four = Bst.new 4
     four.insert 5
     assert_equal [4, 5], record_all_data(four)
   end
 
   def test_iterating_over_complex_tree
-    skip
     four = Bst.new 4
     four.insert 2
     four.insert 1
